3482f725f45937843683d08ca873f0e9a1a62fa1
Fabio Bedini <io@ilwebdifabio.it>
2018-07-22 10:04:24 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iwdf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'iwdf';
+}
