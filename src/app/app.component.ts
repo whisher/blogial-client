571b3901d85295ec6a0675b0967c55cbd9aaa807
Fabio Bedini <io@ilwebdifabio.it>
2018-08-16 08:47:26 +0200

    refactoring admin layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,7 @@ import { Component } from '@angular/core';
 import { PwaService } from './shared/pwa/pwa.service';
 @Component({
   selector: 'iwdf-root',
-  template: `<router-outlet></router-outlet>`,
-  styles: [`
-    :host {
-      display: flex;
-      flex: 1 0 auto;
-      flex-direction: column;
-      background: #fff;
-    }
-  `]
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   constructor(private pwa: PwaService){}
